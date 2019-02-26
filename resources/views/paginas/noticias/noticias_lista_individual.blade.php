810e23e01b2bded395d7d51faa74f9dc31583497
Mauricio Costanzo <ventas@worldmaster.com.uy>
2019-02-26 16:26:51 -0300

    init

---
+++ resources/views/paginas/noticias/noticias_lista_individual.blade.php
@@ -0,0 +1,15 @@
+        <div class="post-preview">
+          <a href=" {{$Noticia->route}}">
+            <h2 class="post-title">
+              {{$Noticia->name}} 
+            </h2>
+            <h3 class="post-subtitle">
+                {{$Noticia->sub_name}} 
+            </h3>
+          </a>
+          <p class="post-meta">Posted by
+            <a href="#">Start Bootstrap</a>
+            on September 24, 2019
+          </p>
+        </div>
+        <hr class="post-hr">
