1457d8176bcbee1480ff321e817b5d6c604fdf1f
Mauricio Costanzo <ventas@worldmaster.com.uy>
2019-02-27 12:15:36 -0300

    fg

--- app/Http/Rutas/CV/Ruta_Cv.php
+++ app/Http/Rutas/CV/Ruta_Cv.php
@@ -8,7 +8,7 @@ Route::get('get_cv',
 ]); 
 
 
-Route::pathc('patch_cv',
+Route::pach('patch_cv',
 [
   'uses'  => 'Admin_Empresa\Admin_Datos_Corporativos_Controller@patch_cv',
   'as'    => 'patch_cv'
