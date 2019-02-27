0c088bceb7c4d9f2252a293494e5ba26e19e36c0
Mauricio Costanzo <ventas@worldmaster.com.uy>
2019-02-27 12:22:13 -0300

    df

--- resources/views/admin/cv/cv_editar.blade.php
+++ resources/views/admin/cv/cv_editar.blade.php
@@ -25,14 +25,47 @@
 
       {{-- datos corporativos --}}
       <div class="contenedor-grupo-datos">
-        <div class="contenedor-grupo-datos-titulo"><span class="icon-person"></span> Cv</div>
+        <div class="contenedor-grupo-datos-titulo">Cv</div>
         <div class="contenedor-formulario-label-fiel">                       
           @include('admin.cv.formularios_partes.datos_user')
         </div>
       </div>
 
-      
+       {{-- datos corporativos --}}
+      <div class="contenedor-grupo-datos">
+        <div class="contenedor-grupo-datos-titulo">Imagenes</div>
+        <div class="contenedor-formulario-label-fiel">                       
+            <div class="formulario-label-fiel">
+            {!! Form::label('img1', 'Imagen cv 1', array('class' => 'formulario-label ')) !!}
+            {!! Form::file('img1',['class' => 'formulario-field']) !!}   
+            </div>
 
+            <div class="formulario-label-fiel">
+            {!! Form::label('img2', 'Imagen cv 2', array('class' => 'formulario-label ')) !!}
+            {!! Form::file('img2',['class' => 'formulario-field']) !!}   
+            </div>
+
+            <div class="formulario-label-fiel">
+            {!! Form::label('img3', 'Imagen cv 3', array('class' => 'formulario-label ')) !!}
+            {!! Form::file('img3',['class' => 'formulario-field']) !!}   
+            </div>
+
+            <div class="formulario-label-fiel">
+            {!! Form::label('img4', 'Imagen cv 4', array('class' => 'formulario-label ')) !!}
+            {!! Form::file('img4',['class' => 'formulario-field']) !!}   
+            </div>
+
+            <div class="flex-row-column get_width_100" >
+              <img class="admin-img-section-img" src="{{$Empresa->img_cv_patch}}1.jpg">
+              <img class="admin-img-section-img" src="{{$Empresa->img_cv_patch}}2.jpg">
+              <img class="admin-img-section-img" src="{{$Empresa->img_cv_patch}}3.jpg">
+              <img class="admin-img-section-img" src="{{$Empresa->img_cv_patch}}4.jpg">
+            </div>
+
+        </div>
+      </div>
+
+    
       
 
       
