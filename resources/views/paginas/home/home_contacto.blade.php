ef75054457060cb00fcbb08d9e6c4a83303e7cb1
Mauricio Costanzo <ventas@worldmaster.com.uy>
2019-02-27 18:15:31 -0300

    df

--- resources/views/paginas/home/home_contacto.blade.php
+++ resources/views/paginas/home/home_contacto.blade.php
@@ -1,9 +1,9 @@
 <section class="BackgroundGris" id="contact">
       <div class="flex-row-center flex-wrap get_width_100 flex-justifice-space-around">
         <div class="contiene_contacto_img">
-          <img src="{{ url() }}/imagenes/team/mauri1.jpg" class="contacto_img"> 
+          <img src="{{ url() }}/imagenes/team/1.jpg" class="contacto_img"> 
           <div class="contacto_name">Mauricio</div>
-          <div class="contacto_aclaracion color-text-gris"> Estoy encargado de atenderte en lo que necesites..</div>
+          <div class="contacto_aclaracion color-text-gris"> Contáctame! estoy siempre a la orden</div>
           @if($Empresa->whatsapp_empresa != 'no')
                   <a class="footer-icono-social" href="{{$Empresa->link_whatsapp_send}}"> 
                     <i class="fab fa-whatsapp"></i>
