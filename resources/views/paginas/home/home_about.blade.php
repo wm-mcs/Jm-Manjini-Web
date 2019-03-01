63c89c3aa9a7e5007267bfe8e83607006e1c9b09
Mauricio Costanzo <ventas@worldmaster.com.uy>
2019-03-01 15:04:57 -0300

    rt

--- resources/views/paginas/home/home_about.blade.php
+++ resources/views/paginas/home/home_about.blade.php
@@ -6,10 +6,21 @@
       <div class="container">
         <div class="row">
           <div class="col-lg-8 mx-auto text-center">
-            <h2 class="section-heading text-white">{ ¿Problema a solucionar? }</h2>
+            <h2 class="section-heading text-white">Sobre mi</h2>
             
             <hr class="light my-4">
-            <p class="text-white mb-4">¿Te gustaría que te ayude a soluciona {el problema a solucionar mencionado}?. { Explicar de que trata el servicio y comunicar el valor que aporta }
+            <p class="text-white mb-4"> <strong>¿Quien soy?</strong> : Licenciado en Psicología, Psicoanalista Javier Jesús Mangini Colombo; Ex-Docente de la UdelaR, Facultad de Psicología dentro del Instituto de Psicología de la Salud.
+
+            Psicólogo clínico en Montevideo en consultorio particular, esto no solo implica terapia propiamente dicha sino que también la realización de informes Diagnostico, Psicopedagogicos, Pedagógicos y Orientación Vocacional, crisis de pareja, etc .
+            </p>
+
+            <p class="text-white mb-4">Investigador de temas relacionados a la Violencia basada en Género,especialmente Domestica e intra-familiar.
+            Selección de personal en Recursos Humanos, actualmente cursando diplomado en difucultades de aprendizaje.
+
+            Conferenciante en congresos internacionales en México, Argentina y Chile de temáticas de Violencia basada en genero y las consecuencias Psicológicas que esto puede traer.
+            </p>
+
+             <p class="text-white mb-4">Psicologo comprometido con su trabajo, en constante formación con el fin de formar y ser formado en una dialéctica de conocimientos.
             </p>
             
           </div>
