f87e734d893c43b0ba7ee95fc423921a92c51292
Mauricio Costanzo <ventas@worldmaster.com.uy>
2019-03-07 20:06:44 -0300

    fg

--- resources/views/admin/cv/formularios_partes/datos_user.blade.php
+++ resources/views/admin/cv/formularios_partes/datos_user.blade.php
@@ -1,4 +1,5 @@
 <div class="formulario-label-fiel">
+  @include('admin.noticias.formularios_partes.aclaracion_etiquetas')
   {!! Form::label('cv_text', 'CV', array('class' => 'formulario-label ')) !!}
   {!! Form::textarea('cv_text', null ,['class' => 'formulario-field',
                                            'cols'    => '14',
