f3563282ea9b8b2260209834a440139bcdb2e45f
Mauricio Costanzo <ventas@worldmaster.com.uy>
2019-03-07 20:02:17 -0300

    df

---
+++ resources/views/admin/noticias/formularios_partes/aclaracion_etiquetas.blade.php
@@ -0,0 +1,16 @@
+<div class="aclaracion-de-etiquetas">
+
+  <div class="aclaracion-de-etiquetas-linea"> Títulos : <span class="text-bold"> (T) (/T) </span> </div>
+
+  <div class="aclaracion-de-etiquetas-linea"> Párrafos: <span class="text-bold"> (P) (/P) </span> </div>
+
+  <div class="aclaracion-de-etiquetas-linea"> Links: <span class="text-bold"> (A) Ejemplo: http://www.coloquiomasculinidades.cl/ponencias-para-descargar(/A) (AT) click para ver (/AT) </span> </div>
+
+  <div class="aclaracion-de-etiquetas-linea"> Imagen: <span class="text-bold"> (IMG) Ejemplo: http://javiermanjini.worldmaster.com.uy/imagenes/Cv/Webs-cv-2.jpg  (/IMG) </span> </div>
+
+    <div class="aclaracion-de-etiquetas-linea"> Pie de imagen: <span class="text-bold"> (IMGT)  Javier J. Mangini Colombo (/IMGT)</span> </div>
+    
+  
+</div>
+
+
