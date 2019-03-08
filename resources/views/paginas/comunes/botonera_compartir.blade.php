5a27c0ab38cd8d6ef1d8f80923524dda22c90cf3
Mauricio Costanzo <ventas@worldmaster.com.uy>
2019-03-07 23:23:48 -0300

    df

---
+++ resources/views/paginas/comunes/botonera_compartir.blade.php
@@ -0,0 +1,12 @@
+
+
+
+{{-- //whatzap icono --}}
+<a class="no-mostrar-barra-de-carga icono-compartir-whatzap" href="whatsapp://send?text={{$Entidad->route}}" data-action="share/whatsapp/share">
+        <span class="icon-share"></span>
+</a>
+
+
+<a class="no-mostrar-barra-de-carga icono-compartir-whatzap" href="http://facebook.com/sharer.php?u={{$Entidad->route}}">
+        <span class="icon-share"></span>
+</a>
