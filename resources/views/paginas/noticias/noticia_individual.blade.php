f270a9523f44f8877efb9a8693a064675359764e
Mauricio Costanzo <ventas@worldmaster.com.uy>
2019-03-09 14:17:14 -0300

    df

--- resources/views/paginas/noticias/noticia_individual.blade.php
+++ resources/views/paginas/noticias/noticia_individual.blade.php
@@ -35,6 +35,8 @@
 
  <div class="flex-row-center">
 
+     <i class="fas fa-share-alt"></i>
+
     {{-- //whatzap icono --}}
     <a class="no-mostrar-barra-de-carga icono-compartir-whatzap" href="whatsapp://send?text={{$Noticia->route}}" data-action="share/whatsapp/share">
             <span class="icon-share"></span>
