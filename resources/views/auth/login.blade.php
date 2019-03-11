fba4e9d44df0756829223c8903658205491efc9c
Mauricio Costanzo <ventas@worldmaster.com.uy>
2019-03-10 22:58:44 -0300

    df

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -20,20 +20,9 @@
 
 
  
-{{-- ejemplo para la bavehacion --}}
-<div class="admin-contnedor-navegacion-miga">
-  {{-- home --}}
-  <a href="{{route('get_home')}}"><span class="icon-home"></span></a>
+<div class="wraper-content-principal-con-nav">
 
-  {{-- separador --}}
-  <span class="spam-separador"><span class="icon-keyboard_arrow_right"></span></span> 
-    
-  {{-- lugar donde esta --}}
-  <span>Iniciar Sesión</span>
-</div>
-
-
-  <h1>Inicio de Sesión</h1>
+   <h1 class="mt-2">Inicio de Sesión</h1>
    <div class="container-fluid section-wrapper">
      <div class="row">    
       <div class="col-sm-8 col-sm-push-2 col-md-6 col-md-push-3 col-lg-4 col-lg-push-4 wow fadeInUp">
@@ -41,6 +30,11 @@
       </div>
     </div>
   </div>
+  
+</div>
+
+
+  
      
 
 @stop
