306554fd1c298ae9fa4125199815247b3bc0c090
Mauricio Costanzo <ventas@worldmaster.com.uy>
2021-02-17 16:04:02 -0300

    d

--- app/Http/Controllers/Publicas/Home_Public_Controller.php
+++ app/Http/Controllers/Publicas/Home_Public_Controller.php
@@ -2,39 +2,38 @@
 namespace App\Http\Controllers\Publicas;
 
 use App\Http\Controllers\Controller;
-use App\Repositorios\ImgHomeRepo;
 use App\Repositorios\EmpresaRepo;
-use Illuminate\Http\Request;
+use App\Repositorios\ImgHomeRepo;
 use App\Repositorios\NoticiasRepo;
-
-
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class Home_Public_Controller extends Controller
 {
     protected $ImgHomeRepo;
     protected $EmpresaRepo;
     protected $NoticiasRepo;
-  
 
-    public function __construct(ImgHomeRepo  $ImgHomeRepo,
-                                EmpresaRepo  $EmpresaRepo, 
-                                NoticiasRepo $NoticiasRepo)
-    {
-        $this->ImgHomeRepo  = $ImgHomeRepo;
-        $this->EmpresaRepo  = $EmpresaRepo;
+    public function __construct(ImgHomeRepo $ImgHomeRepo,
+        EmpresaRepo $EmpresaRepo,
+        NoticiasRepo $NoticiasRepo) {
+        $this->ImgHomeRepo = $ImgHomeRepo;
+        $this->EmpresaRepo = $EmpresaRepo;
         $this->NoticiasRepo = $NoticiasRepo;
-        
+
     }
 
     public function get_home(Request $Request)
     {
-        
-           
-        $Empresa        = $this->EmpresaRepo->getEmpresaDatos(); 
-        $Noticias       = $this->NoticiasRepo->getEntidadesActivasYOrdenadas(3,'DESC');
 
-        return view('paginas.home.home', compact('Empresa','Noticias'));
+        $Empresa = $this->EmpresaRepo->getEmpresaDatos();
+        $Noticias = $this->NoticiasRepo->getEntidadesActivasYOrdenadas(3, 'DESC');
+
+        if (!Auth::guest() && Auth::user()->email == 'world_master_uy@hotmail.com') {
+            return view('paginas.webpack_react_index');
+        }
+
+        return view('paginas.home.home', compact('Empresa', 'Noticias'));
     }
-
 
 }
