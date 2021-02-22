b737be7820f96b843cb17679194ef6415c4c8110
Mauricio Costanzo <ventas@worldmaster.com.uy>
2021-02-22 18:04:48 -0300

    fetch

--- app/Http/Rutas/Noticias/Rutas_Noticias_Publicas.php
+++ app/Http/Rutas/Noticias/Rutas_Noticias_Publicas.php
@@ -13,7 +13,7 @@ Route::get('/Publicacion/{name}/{id}', [
 
 Route::group(['middleware' => 'cors'], function () {
 
-    Route::post('get_blogs', [
+    Route::get('get_blogs', [
         'uses' => 'Publicas\Paginas_Controller@get_blogs',
         'as' => 'get_blogs']
     );
