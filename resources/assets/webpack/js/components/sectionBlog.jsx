4e7c7e65f26c49f6e0487aa1d65e5b7d35fbdac5
Mauricio Costanzo <mauricio@worldmaster.com.uy>
2021-02-22 20:21:15 -0300

    fetch

--- resources/assets/webpack/js/components/sectionBlog.jsx
+++ resources/assets/webpack/js/components/sectionBlog.jsx
@@ -1,30 +1,42 @@
 import React, { useState, useEffect } from 'react';
 import Axios from 'axios';
+
 const SectionBlog = () => {
   const [loading, setLoading] = useState(true);
   const [blogs, setBlogs] = useState([]);
   const [loadMore, setLoadMore] = useState(true);
-  const [idsYaUsados, setIdsYaUsados] = useState([]);
 
   const fetcData = () => {
     if (!loadMore) {
       return 'hola';
     }
 
-    return Axios.get('https://psicologojaviermangini.com.uy/get_blogs')
-      .then(function (response) {
-        let data = response.data;
-        if (data.Data.length == 0) {
+    let idsYaUsados = blogs.map((obj) => obj.id);
+
+    idsYaUsados = idsYaUsados.join();
+
+    return fetch(
+      `https://psicologojaviermangini.com.uy/get_blogs_ids:${idsYaUsados}`
+    )
+      .then((response) => {
+        if (!response.ok) {
+          throw Error(response.statusText);
+        }
+
+        return response.json();
+      })
+      .then((responseAsJson) => {
+        const { data } = responseAsJson;
+        if (data.Data.length === 0) {
           setLoadMore(false);
         } else {
           setBlogs(blogs.concat(data.Data));
         }
-
         setLoading(false);
       })
-      .catch(function (error) {
+      .catch((error) => {
         setLoading(false);
-        console.log(error.message);
+        console.log('Looks like there was a problem: \n', error);
       });
   };
 
