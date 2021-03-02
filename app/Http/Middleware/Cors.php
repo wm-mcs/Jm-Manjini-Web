b4019f551d29273b777c5469d88ed4511d467b84
Mauricio <ventas@worldmaster.com.uy>
2021-03-01 21:50:32 -0300

    df

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -10,11 +10,24 @@ class Cors
         $request,
         Closure $next
     ) {
+        header("Access-Control-Allow-Origin: *");
+
+        // ALLOW OPTIONS METHOD
+        $headers = [
+            'Access-Control-Allow-Methods' => 'POST, GET, OPTIONS, PUT, DELETE',
+            'Access-Control-Allow-Headers' => 'Content-Type, X-Auth-Token, Origin',
+        ];
+
+        if ($request->getMethod() == "OPTIONS") {
+            // The client-side application can set only headers allowed in Access-Control-Allow-Headers
+            return Response::make('OK', 200, $headers);
+        }
 
         $response = $next($request);
-        $response->header('Access-Control-Allow-Methods', 'HEAD, GET, POST, PUT, DELETE');
-        $response->header('Access-Control-Allow-Headers', $request->header('Access-Control-Request-Headers'));
-        $response->header('Access-Control-Allow-Origin', '*');
+
+        foreach ($headers as $key => $value) {
+            $response->header($key, $value);
+        }
 
         return $response;
     }
